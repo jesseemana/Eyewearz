8f0883b2e78eedc4486c087b90bf047904dc8e2e
Jesse Emana <jesseemana@gmail.com>
2023-12-16 00:49:05 +0200

    REFACTOR: recreated card with new styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { GlassProps } from '../types'
+import { ProductProps } from '../types'
 
-const ProductCard = ({ item }:{ item: GlassProps }) => {
+const ProductCard = ({ item }:{ item: ProductProps }) => {
   return (
-    <Link
-      to={`/product/${item.id}`}
-      className='border rounded-md w-[250px] h-[230px] hover:shadow-lg hover:cursor-pointer hover:transition-all'
-    >
-      <img src={item.image} alt={item.name} className='p-4'/>
-      <div className='flex flex-col p-2'>
-        <p className='text-gray-800 font-normal'>{item.name}</p>
-        <p className='font-semibold text-gray-700'>{item.price}</p>
-      </div>
-    </Link>
+    <div className=''>
+      <p className='bg-yellow-600 w-10 rounded-full text-white px-2 font-semibold'>hot</p>
+      <Link
+        to={`/product/${item.id}`}
+        className='hover:transition-all mt-2'
+      >
+        <img src={item.image} alt={item.name} className='h-[200px]'/>
+        <div className='flex flex-row p-2 justify-between'>
+          <div className='flex flex-col gap-2'>
+            <p className='text-gray-800 capitalize font-semibold text-sm'>{item.name}</p>
+            <p className='font-bold text-gray-900'>{item.price}</p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <div className='flex gap-2'>
+              <button className='bg bg-blue-400 px-3 text-white rounded-sm'>cart</button>
+              <button className='border border-blue-400 px-3 text-blue-400 rounded-sm'>fav</button>
+            </div>
+            <p className='text text-gray-400 text-sm'>{item.reviews} reviews</p>
+          </div>
+        </div>
+      </Link>
+    </div>
   )
 }
 
