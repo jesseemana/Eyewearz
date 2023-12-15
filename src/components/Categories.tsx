ebb940b879a7c6f73a7ebd07cceb67c56c344deb
Jesse Emana <jesseemana@gmail.com>
2023-12-15 22:44:48 +0200

    CHORE: created new hero section and category section

---
+++ src/components/Categories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className='grid grid-cols-3 lg:grid-cols-4 place-items-center py-10'>
+      {categories.map((item, index) => (
+        <div key={index} className='flex flex-col items-center'>
+          <img src={item.image} alt="" className='lg:h-40 h-20' />
+          <p className='capitalize font-semibold text-gray-800'>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
+
+export const categories = [
+  {
+    'name': 'women',
+    'image': './women.webp'
+  },
+  {
+    'name': 'men',
+    'image': './men.webp'
+  },
+  {
+    'name': 'kids',
+    'image': './kids.webp'
+  },
+  {
+    'name': 'sunglasses',
+    'image': './sunglasses.webp'
+  },
+]
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src="./hero.webp" alt="" />
+    </div>
+  )
+}
+
+export default Header
