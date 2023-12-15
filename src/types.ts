1385842166a6307309ea412025ed91cc5a0da2a5
Jesse Emana <jesseemana@gmail.com>
2023-12-16 00:16:28 +0200

    CHORE: added types for reviews

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,14 @@
-export type GlassProps = {
+export type ProductProps = {
   id: number
   name: string
   price: string
   image: string
+  reviews: number
+}
+
+export interface Review {
+  image: string
+  name: string
+  review: string
+  content: string
 }
