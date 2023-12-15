2a79085054e50813f3f9001e1eb0734d55898b24
Jesse Emana <jesseemana@gmail.com>
2023-12-15 22:46:10 +0200

    REFACTOR: refactored component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,16 @@ const Navbar = () => {
   return (
     <nav className="shadow-sm">
       <Container>
-        <nav className='flex justify-between py-4 items-center'>
-          <div>
+        <nav className='flex justify-between py-3 items-center'>
+          <Link to={'/'}>
             <h1>logo</h1>
-          </div>
-          <div className='flex gap-4 items-center capitalize font-semibold text-gray-800'>
-            <Link to={'/'} className="navbar-brand">glasses</Link>
-            <Link to={'/'} className="navbar-brand">eye tests</Link>
-            <Link to={'/'} className="navbar-brand">offers</Link>
+          </Link>
+          <div className='lg:flex gap-4 items-center capitalize font-normal text-gray-800 hidden'>
+            <Link to={'/eyeglasses'} className="navbar-brand">eyeglasses</Link>
+            <Link to={'/sunglasses'} className="navbar-brand">sunglasses</Link>
+            <Link to={'/tests'} className="navbar-brand">eye tests</Link>
             <Link to={'/'} className="navbar-brand">store</Link>
+            <Link to={'/'} className="navbar-brand">deals</Link>
           </div>
           <button className='bg-blue-400 py-2 px-4 rounded-full capitalize font-semibold text-white'>
             book appointment
