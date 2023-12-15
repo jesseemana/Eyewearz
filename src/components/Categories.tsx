c4bbfa84519c91aad52d65a84e0f78afc57747d3
Jesse Emana <jesseemana@gmail.com>
2023-12-16 00:50:06 +0200

    REFACTOR: added extra category

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Categories = () => {
   return (
-    <div className='grid grid-cols-3 lg:grid-cols-4 place-items-center py-10'>
+    <div className='grid grid-cols-3 lg:grid-cols-5 place-items-center py-10'>
       {categories.map((item, index) => (
         <div key={index} className='flex flex-col items-center'>
           <img src={item.image} alt="" className='lg:h-40 h-20' />
@@ -17,19 +17,23 @@ export default Categories
 
 export const categories = [
   {
-    'name': 'women',
-    'image': './women.webp'
+    name: 'women',
+    image: './women.webp'
   },
   {
-    'name': 'men',
-    'image': './men.webp'
+    name: 'men',
+    image: './men.webp'
   },
   {
-    'name': 'kids',
-    'image': './kids.webp'
+    name: 'kids',
+    image: './kids.webp'
   },
   {
-    'name': 'sunglasses',
-    'image': './sunglasses.webp'
+    name: 'sunglasses',
+    image: './sunglasses.webp'
+  },
+  {
+    name: 'bluelight',
+    image: './bluelight.webp'
   },
 ]
