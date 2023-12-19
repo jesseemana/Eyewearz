13e7755b22fb1339b582820273cde5e89bac123b
Jesse Emana <jesseemana@gmail.com>
2023-12-19 22:45:37 +0200

    CHORE: components completed

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Categories = () => {
   return (
     <div className='grid grid-cols-3 lg:grid-cols-5 place-items-center py-10'>
       {categories.map((item, index) => (
-        <div key={index} className='flex flex-col items-center'>
-          <img src={item.image} alt="" className='lg:h-40 h-20' />
+        <Link 
+          key={index} 
+          to={item.name} 
+          className='flex flex-col items-center'
+        >
+          <img src={item.image} alt={item.name} className='lg:h-40 h-20' />
           <p className='capitalize font-semibold text-gray-800'>{item.name}</p>
-        </div>
+        </Link>
       ))}
     </div>
   )
--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -23,17 +23,20 @@ const Explore = () => {
   return (
     <Container>
       <div className='flex flex-col items-center gap-4 py-'>
-        <h1 className='text-4xl font-semibold text-gray-800'>
-          Check out our available frames
+        <h1 className='lg:text-4xl text-2xl font-semibold text-gray-800 capitalize text-center'>
+          check out our featured frames
         </h1>
-        <p className='text-gray-700 text-[20px]'>
+        <p className='text-gray-700 text-[17px] lg:text-[20px] text-center'>
           Explore our wide range of eyewear. We may have what you're looking for.
         </p>
       </div>
 
       <div className='grid grid-cols-2 md:grid-cols-3 place-items-center gap-10 py-5'>
         {glasses.map((item: ProductProps) => (
-          <ProductCard key={item.id} item={item} />
+          <ProductCard 
+            item={item} 
+            key={item.id} 
+          />
         ))}
       </div>
     </Container>
