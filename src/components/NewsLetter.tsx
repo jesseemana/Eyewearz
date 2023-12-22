a4090d515c14444255077f3f5be17626f3510ae1
Jesse Emana <jesseemana@gmail.com>
2023-12-22 15:04:01 +0200

    REFACTOR: added responsiveness to component

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -4,35 +4,34 @@ import { FaFacebook, FaTwitter, FaTiktok, FaYoutube, FaInstagram } from 'react-i
 
 const NewsLetter = () => {
   return (
-    <section className='py-10'>
+    <section id='newsletter' className='py-10'>
       <Container>
         <div className='flex flex-col items-center'>
-          <h1 className='text-4xl font-semibold text-gray-800 capitalize'>
+          <h1 className='text-2xl lg:text-4xl font-semibold text-gray-800 capitalize text-center'>
             professional stylish glasses at affordable prices
           </h1>
-          <p className='text-gray-700 text-[18px] text-center'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br/>
-            Officia quis sint nulla perspiciatis dignissimos doloribus assumenda 
-            commodi dolore nobis fugit<br/>
-            odio aperiam explicabo mollitia tempora blanditiis.<br/>
+          <p className='text-sm text-gray-700 md:text-[18px] text-center'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br/>
+            Officia quis sint nulla perspiciatis dignissimos doloribus assumenda<br/>
+            commodi dolore nobis fugit.
           </p>
         </div>
-        <div className='py-10 flex items-center gap-4 justify-center'>
-          <p className='capitalize font-semibold text-2xl'>join us now and get 15% off</p>
-          <div>
+        <div className='py-10 flex items-center gap-4 justify-center flex-col lg:flex-row'>
+          <p className='capitalize font-semibold md:text-2xl text-xl'>join us now and get 15% off</p>
+          <div className='flex'>
             <input 
               type='text' 
               placeholder='enter your email address' 
-              className='px-4 py-2 rounded-sm border border-black h-10 w-[300px]' 
+              className='px-4 py-2 rounded-sm border border-black h-10 lg:w-[300px]' 
             />
             <button className='bg-black text-white py-2 px-2 rounded-sm'>subscribe</button>
           </div>
-          <div className='flex items-center gap-4 '>
-            <FaFacebook size={30} />
-            <FaTwitter size={30} />
-            <FaInstagram size={30} />
+          <div className='flex items-center gap-6 sm:hidden md:flex'>
+            <FaFacebook size={30} className='fill-blue-500' />
+            <FaTwitter size={30} className='fill-blue-400' />
+            <FaInstagram size={30} className='fill-purple-500' />
             <FaTiktok size={30} />
-            <FaYoutube size={30} />
+            <FaYoutube size={30} className='fill-red-600'/>
           </div>
         </div>
       </Container>
