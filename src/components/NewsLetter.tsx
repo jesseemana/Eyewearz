ed874983aee255d0164a0e552e98edd2c615e24f
Jesse Emana <jesseemana@gmail.com>
2023-12-22 15:50:20 +0200

    CHORE: component complete

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Container from './Container'
-import { FaFacebook, FaTwitter, FaTiktok, FaYoutube, FaInstagram } from 'react-icons/fa'
+import Socials from './Socials'
 
 const NewsLetter = () => {
+  const doStuff = () => {
+    alert('Subscribed to newsletter!')
+  }
+
   return (
     <section id='newsletter' className='py-10'>
       <Container>
@@ -17,21 +21,23 @@ const NewsLetter = () => {
           </p>
         </div>
         <div className='py-10 flex items-center gap-4 justify-center flex-col lg:flex-row'>
-          <p className='capitalize font-semibold md:text-2xl text-xl'>join us now and get 15% off</p>
+          <p className='capitalize font-semibold md:text-2xl text-xl text-center'>join us now and get 15% off</p>
           <div className='flex'>
             <input 
               type='text' 
               placeholder='enter your email address' 
               className='px-4 py-2 rounded-sm border border-black h-10 lg:w-[300px]' 
             />
-            <button className='bg-black text-white py-2 px-2 rounded-sm'>subscribe</button>
+            <button 
+              onClick={doStuff} 
+              className='bg-black text-white py-2 px-4 rounded-sm'
+            >
+              subscribe
+            </button>
           </div>
-          <div className='flex items-center gap-6 sm:hidden md:flex'>
-            <FaFacebook size={30} className='fill-blue-500' />
-            <FaTwitter size={30} className='fill-blue-400' />
-            <FaInstagram size={30} className='fill-purple-500' />
-            <FaTiktok size={30} />
-            <FaYoutube size={30} className='fill-red-600'/>
+          {/* use tailwind merge for custom display or hidden */}
+          <div className='hidden md:flex'> 
+            <Socials />
           </div>
         </div>
       </Container>
