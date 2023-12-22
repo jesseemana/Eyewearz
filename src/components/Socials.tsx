cc88c3f6941a0f11ead89b9659f313e1ad65cd6d
Jesse Emana <jesseemana@gmail.com>
2023-12-22 17:29:19 +0200

    REFACTOR: changed the domain names

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { FaFacebook, FaTwitter, FaTiktok, FaYoutube, FaInstagram } from 'react-icons/fa'
 
 const Socials = () => {
   return (
     <div className='flex items-center gap-6'>
       <a href='https://www.facebook.com/mybusinesspage' target='blank'>
-        <FaFacebook size={30} className='fill-blue-500' />
+        <FaFacebook 
+          size={30} 
+          className='fill-blue-500' 
+        />
       </a>
-      <Link to={'www.twitter.com/mybusinesspage'} target='blank'>
-        <FaTwitter size={30} className='fill-blue-400' />
-      </Link>
-      <a href='https://www.instagram.com/mybusinesspage'>
-        <FaInstagram size={30} className='fill-purple-500' />
+      <a href='https://www.twitter.com/mybusinesspage' target='blank'>
+        <FaTwitter 
+          size={30} 
+          className='fill-blue-400' 
+        />
       </a>
-      <a href='https://www.tiktok.com/mybusinesspage'>
+      <a href='https://www.instagram.com/mybusinesspage' target='blank'>
+        <FaInstagram 
+          size={30} 
+          className='fill-purple-500' 
+        />
+      </a>
+      <a href='https://www.tiktok.com/mybusinesspage' target='blank'>
         <FaTiktok size={30} />
       </a>
-      <a href='https://www.youtube.com/mybusinesspage'>
-        <FaYoutube size={30} className='fill-red-600'/>
+      <a href='https://www.youtube.com/mybusinesspage' target='blank'>
+        <FaYoutube 
+          size={30} 
+          className='fill-red-600'
+        />
       </a>
     </div>
   )
