4cbb46e43f0cccc004c41b6feba10078d934815d
Jesse Emana <jesseemana@gmail.com>
2024-01-01 09:13:17 +0200

    CHORE: created loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './Container'
+
+const Loading = () => {
+  return (
+    <Container>
+      <p className='py-4'>Loading...</p>
+    </Container>
+  )
+}
+
+export default Loading
